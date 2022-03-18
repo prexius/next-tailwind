540759bad5a3b19fc2bd1fcf2afbab9454232375
imsaifun <imsaifun@gmail.com>
2022-03-18 12:17:22 +0600

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
-        extend: {},
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            xxl: "1400px"
+        },
+
+        fontFamily: {
+            sans: ["Poppins, sans-serif"]
+        },
+
+        extend: {
+            colors: {
+                "primary": "#0D6EFD",
+                "secondary": "#6C757D",
+                "success": "#198754",
+                "info": "#0DCAF0",
+                "warning": "#FFC107",
+                "danger": "#DC3545",
+            },
+        },
     },
     plugins: [],
 }
